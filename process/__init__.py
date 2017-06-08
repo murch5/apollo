c03a1762bc7b4f94e7bcdfb6227b9aee15d7e971
murch <ryan.c.murchie@gmail.com>
2017-06-07 23:14:01 -0400

    add more imports to __init__.py

---
+++ process/__init__.py
@@ -0,0 +1,6 @@
+from process.process import Process
+from process.io import Export
+from process.array import Slice
+from process.array import Squeeze
+from process.array import Project
+from process.process_manager import ProcessManager
