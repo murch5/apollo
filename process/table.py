55abfa6f6c33866755924fce20e267c157aad295
murch <ryan.c.murchie@gmail.com>
2017-06-11 19:28:43 -0400

    migrate table manip func to new process func group

---
+++ process/table.py
@@ -0,0 +1,32 @@
+from process.process import Process
+import pandas as pd
+
+class GroupByValue(Process):
+
+    def do(self, data):
+
+        axis = self.get("axis")
+        delim = self.get("delim")
+
+        if delim:
+            data = data.str.split(";").apply(pd.Series, 1).stack()
+
+        out = data.groupby(data.iloc[:, 0]).size()
+        out.columns = ["Index", "GroupedCounts"]
+
+        return out
+
+class SubsetByIndexName(Process):
+
+    def do(self,data):
+
+        out = data
+        axis = self.get("axis")
+        subset = self.get("subset")
+
+        if axis in ["col", "c"]:
+            out = data.ix[subset]
+        elif axis in ["row", "r"]:
+            out = data.ix[:, subset]
+
+        return out
