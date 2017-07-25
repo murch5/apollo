87534e8b42ad54f73ababd7333f51a65c8e5e4c2
murch <ryan.c.murchie@gmail.com>
2017-07-25 10:24:18 -0400

    add new Process classes for manipulation arrays
    added Retype, Transect

--- process/array.py
+++ process/array.py
@@ -1,9 +1,17 @@
 from process.process import Process
+import scipy.ndimage.interpolation as interpolation
+import numpy as np
+import pandas as pd
+import logging as logging
 
 class Slice(Process):
 
     def do(self, data):
 
+        logging.debug("------ Slice: shape: " + str(data.shape) + " , type: " + str(type(data)))
+
+        if isinstance(data,pd.DataFrame):
+            data = data.as_matrix()
         axis = self.get("axis")
         set = self.get("slice")
 
@@ -13,6 +21,7 @@ class Slice(Process):
 
         out = data[sli]
 
+        print(out.shape)
         return out
 
 class Squeeze(Process):
@@ -37,5 +46,52 @@ class Project(Process):
         elif func == "mean":
             out = data.mean(axis=axis)
 
+
+        return out
+
+class Retype(Process):
+
+    def do(self,data):
+        out = data
+
+        type = self.get("type")
+
+        out= data.astype(type)
+
+
+        return out
+
+class Transect(Process):
+
+    def do(self,data):
+        out = data
+
+        mode = self.get("mode")
+        axis_one = self.get("axis_one")
+        axis_two = self.get("axis_two")
+        line_start = self.get("line_start")
+        line_end = self.get("line_end")
+
+        dist = self.get("dist")
+
+        x = np.linspace(line_start[0],line_end[0],dist)
+        y = np.linspace(line_start[1], line_end[1], dist)
+
+        val_index = np.arange(0,dist)
+
+        project = self.get("project")
+
+        if project:
+            data_accum = []
+            for i,slice in enumerate(data):
+                slice_interpolate = interpolation.map_coordinates(slice.T, np.stack((x, y)))
+                data_accum.append(slice_interpolate)
+            val = pd.DataFrame(data_accum)
+
+        else:
+            val_interpolate = interpolation.map_coordinates(data.T, np.stack((x, y)))
+            val = {"index": val_index, "value": val_interpolate}
+
+        out = val
 
         return out
