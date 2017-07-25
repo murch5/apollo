f7c93ab2ddba57294fd509334e838b92e38a2319
murch <ryan.c.murchie@gmail.com>
2017-07-25 10:24:41 -0400

    update Export process class to use SciKit Image TIFFwriter

--- process/io.py
+++ process/io.py
@@ -1,6 +1,7 @@
 from process.process import Process
 
 import skimage.io as skimage_io
+import skimage.external.tifffile as tff
 
 class Export(Process):
 
@@ -8,7 +9,10 @@ class Export(Process):
         out = data
 
         output_file = "./output/" + self.get("file_name") + ".tif"
-        skimage_io.imsave(output_file, data, plugin="tifffile")
+        #skimage_io.imsave(output_file, data, plugin="tifffile")
         #logger.debug("--- Export: Save image " + self.name + ".tif")
+
+        with tff.TiffWriter(output_file) as tif:
+            tif.save(out)
 
         return out
