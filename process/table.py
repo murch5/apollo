187a190a6acc2ef9c1e16bcaafcdbd27ad91fd43
murch <ryan.c.murchie@gmail.com>
2017-09-13 12:16:33 -0400

    add SortByIndexLabel, Reindex, and Filter processes

--- process/table.py
+++ process/table.py
@@ -11,10 +11,8 @@ class GroupByValue(Process):
             data = pd.Series(data)
             data = data.str.split(";").apply(pd.Series, 1).stack()
 
-        out = data.groupby(data.iloc[:,0]).size()
+        out = data.groupby(data).size()
         out.columns = ["Index", "GroupedCounts"]
-
-
 
         return out
 
@@ -26,9 +24,51 @@ class SubsetByIndexName(Process):
         axis = self.get("axis")
         subset = self.get("subset")
 
-        if axis in ["col", "c"]:
+        if axis in ["row", "r"]:
+
             out = data.ix[subset]
-        elif axis in ["row", "r"]:
+
+        elif axis in ["col", "c"]:
             out = data.ix[:, subset]
+
+        return out
+
+class SortByIndexLabel(Process):
+
+    def do(self,data):
+
+        out = data
+        ascending = self.get("ascending")
+
+
+        out = data.sort_index(axis = 0,ascending = ascending)
+
+        return out
+
+class Reindex(Process):
+
+    def do(self,data):
+        out = data
+
+        index_new = self.get("indices")
+
+        out = data.reindex(index_new)
+        return out
+
+
+class Filter(Process):
+
+    def do(self, data):
+
+        out = data
+
+        axis = self.get("axis")
+        items = self.get("items")
+        like = self.get("like")
+
+        if items:
+            out = data.filter(items = items, axis = axis)
+        else:
+            out = data.filter(like = like, axis = axis)
 
         return out
