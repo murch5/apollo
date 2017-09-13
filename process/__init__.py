c4f2181009cee3b6379bd4bf0a8864420b90fe50
murch <ryan.c.murchie@gmail.com>
2017-09-13 12:16:56 -0400

    add new process imports to __init__.py

--- process/__init__.py
+++ process/__init__.py
@@ -7,7 +7,12 @@ from process.array import Squeeze
 from process.array import Project
 from process.array import Retype
 from process.array import Transect
+from process.array import Iterate_by_axis
 from process.table import GroupByValue
+from process.table import SubsetByIndexName
+from process.table import SortByIndexLabel
+from process.table import Reindex
+from process.table import Filter
 from process.signal import FFT
 from process.signal import FFT_2D
 from process.signal import Periodogram
