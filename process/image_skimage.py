639edf2e7a21b7fb58aef9abf0d5174d0b500c24
murch <ryan.c.murchie@gmail.com>
2017-10-17 10:53:58 -0400

    add measure.label func from skimage

--- process/image_skimage.py
+++ process/image_skimage.py
@@ -1,7 +1,8 @@
 from process.process import Process
 
 import skimage.morphology as morph
-
+import skimage.measure as measure
+import pandas as pd
 
 class Skeletonize(Process):
 
@@ -9,5 +10,25 @@ class Skeletonize(Process):
         out = data
 
         out = morph.skeletonize(data)
+
+        return out
+
+
+class LabelRegions(Process):
+
+    def do(self,data):
+        out = data
+
+        labelled_regions = measure.label(data)
+
+        regions_properties = measure.regionprops(labelled_regions)
+
+        regions_list = [region.bbox for region in regions_properties]
+
+        regions_list = pd.DataFrame(regions_list)
+
+        regions_list.to_csv("process/regions.csv", header=None,index=False)
+
+        out = data
 
         return out
